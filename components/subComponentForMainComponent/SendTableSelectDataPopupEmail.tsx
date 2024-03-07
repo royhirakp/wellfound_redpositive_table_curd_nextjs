7592c80ef3075637a09e0c72bf32fc3d53f57e67
Hirak Roy <royhirakp@gmail.com>
2024-03-08 01:46:20 +0530

    change mail headding

--- components/subComponentForMainComponent/SendTableSelectDataPopupEmail.tsx
+++ components/subComponentForMainComponent/SendTableSelectDataPopupEmail.tsx
@@ -25,7 +25,7 @@ const SendTableSelectDataPopupEmail: React.FC<
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
       <div className="bg-white p-8 max-w-md mx-auto rounded-md shadow-md">
-        <p className="mb-4">Send mail to "royhirakp@gmail.com"?</p>
+        <p className="mb-4">Send mail to "info@redpositive.in"?</p>
         <div className="flex justify-between">
           <button
             className="bg-blue-500 text-white px-4 py-2 mr-2 rounded-md"
